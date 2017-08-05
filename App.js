e0789384bd9f794015372cdbf1e1e90301dbb40f
Shamarkanda <jalaganga@emergya.com>
2017-08-05 13:06:53 +0200

    Fixed minor error router

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
+'use strict'
+
 let express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    methodOverride = require('methodOverride'),
+    methodOverride = require('method-override'),
     app = express();
 
 //Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
-app.user(bodyParser.json());
+app.use(bodyParser.json());
 app.use(methodOverride());
 
 let router = express.Router();
 router.get('/', (req, res) => {
-    res.send('PRRRRRRRRRRA!');
+    res.send('<h1>PRRRRRRRRRRA!</h1>');
 });
+app.use(router);
 
 app.listen(3000, () => {
     console.log('Node server running on http://localhost:3000');
